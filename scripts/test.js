a08dc1524c48a67b7ef6e670fa2457f29afbc74a
1McFly10 <vlados3702@gmail.com>
2024-06-05 23:35:18 +0200

    add dog

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,13 @@
+let dog = 'Silver'
 let a = 9;
 let b = 15;
+console.log(dog)
 function sum (a, b){
     return a + b;
 }
-console.log(sum(a, b))
+result = sum(a, b)
+console.log(result + dog)
+
 
 
 
